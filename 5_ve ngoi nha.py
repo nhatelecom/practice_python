76ca48cb84c7f0250f756394bfe31a774bd4a9b4
nhatelecom <nhatelecom@gmail.com>
2022-05-09 13:00:13 +0700

    Create 5_ve ngoi nha.py

---
+++ 5_ve ngoi nha.py
@@ -0,0 +1,29 @@
+# [Bài tập] Vẽ ngôi nhà mơ ước
+
+import turtle as pen
+
+pen.pensize(3)
+pen.pencolor("red")
+pen.speed(5)
+
+pen.up()
+pen.goto(-300,0)
+pen.down()
+
+pen.forward(200)
+pen.right(90)
+pen.forward(200)
+pen.right(90)
+pen.forward(200)
+pen.right(90)
+pen.forward(200)
+pen.right(45)
+pen.forward(141)
+pen.right(90)
+pen.forward(141)
+
+pen.up()
+pen.goto(200,200)
+pen.down()
+
+pen.circle(100)
