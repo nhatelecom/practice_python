72bd89d7255bbe4594bed2a737982a67e4ec6973
nhatelecom <nhatelecom@gmail.com>
2022-05-25 09:35:52 +0700

    Update bai_toan_do_nuoc.py

--- bai_toan_do_nuoc.py
+++ bai_toan_do_nuoc.py
@@ -8,9 +8,9 @@
 #     Nếu (a-b)>c thì: Chiều cao nước chứa được tại cột thứ i là: min(a,b) - c
     
 
-block = "31271312"
-highest_block_left_size = 0
-highest_block_right_size = 0
+block = "4030205"
+highest_block_left_side = 0
+highest_block_right_side = 0
 current_block_height = 0
 
 sum_water = 0
@@ -18,20 +18,20 @@ sum_water = 0
 for i in range(1,len(block)-1):
 
     # tìm cột cao nhất bên trái
-    highest_block_left_size = int(block[0])
+    highest_block_left_side = int(block[0])
     for j in range(i):
-        if highest_block_left_size < int(block[j]):
-            highest_block_left_size =int(block[j])
+        if highest_block_left_side < int(block[j]):
+            highest_block_left_side =int(block[j])
 
     # tìm cột cao nhất bên phải
-    highest_block_right_size = int(block[-1])
+    highest_block_right_side = int(block[-1])
     for j in range(i,len(block)):
-        if highest_block_right_size < int(block[j]):
-            highest_block_right_size = int(block[j])
+        if highest_block_right_side < int(block[j]):
+            highest_block_right_side = int(block[j])
     
     # tính lượng nước
     current_block_height = int(block[i])
-    if min(highest_block_left_size,highest_block_right_size) > current_block_height:
-        sum_water = sum_water + min(highest_block_left_size,highest_block_right_size) - current_block_height
+    if min(highest_block_left_side,highest_block_right_side) > current_block_height:
+        sum_water = sum_water + min(highest_block_left_side,highest_block_right_side) - current_block_height
 
 print(sum_water)
