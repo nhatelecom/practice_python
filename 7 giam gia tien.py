2966acf31d14c19cb9e644b3385a93de26aa9c3e
nhatelecom <nhatelecom@gmail.com>
2022-06-17 14:46:27 +0700

    Create 7 giam gia tien.py

---
+++ 7 giam gia tien.py
@@ -0,0 +1,9 @@
+num = int(input("Nhập số tiền đã mua: "))
+if num >= 150:
+	print("Total: ", num - 50)
+elif num >= 100:
+	print("Total: ", num - 25)
+elif num >= 75:
+	print("Total: ", num - 15)
+else:
+print("Total: ", num)
