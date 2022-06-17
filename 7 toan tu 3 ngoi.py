237945f370e6b67e602570e5c09419edb87cbfa2
nhatelecom <nhatelecom@gmail.com>
2022-06-17 10:56:11 +0700

    .

--- 7 toan tu 3 ngoi.py
+++ 7 toan tu 3 ngoi.py
@@ -12,3 +12,13 @@ else:
     msg = "Odd"
 
 print(msg)
+
+"""------------------------------------------------------------------------"""
+a = input()
+a = float(a)
+if a % 2 == 0:
+    print('số chẵn')
+elif a % 2 == 1:
+    print('số lẻ')
+else:
+    print('không phải số tự nhiên')
