7dd55328cff0f438fb1b4c37fd262d88b6abe01f
nhatelecom <nhatelecom@gmail.com>
2022-06-26 16:26:07 +0700

    new

---
+++ 10 ve hinh vuong dung vong for.py
@@ -0,0 +1,16 @@
+import turtle
+
+wn = turtle.Screen()
+
+wn.bgcolor("black")
+wn.title("Square")
+
+myPen = turtle.Turtle()
+myPen.speed(3)
+myPen.color("#FF0000")
+
+for i in range(4):
+    myPen.forward(100)
+    myPen.left(90)
+
+turtle.done()
