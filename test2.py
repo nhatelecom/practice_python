b7bf4a2852d30f4b34719782c4cee62b0fb64a52
nhatelecom <nhatelecom@gmail.com>
2022-06-29 23:55:30 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -11,15 +11,30 @@
 
 
 
-import tkinter as tk
+# import tkinter as tk
 
-def command(event):
-    print("hi")
+# def command(event):
+#     print("hi")
 
-root = tk.Tk()
+# root = tk.Tk()
 
-label = tk.Label(text="Click me!", width=50, height=10, master=root)
-label.pack()
-label.bind("<Button-1>", command)
+# label = tk.Label(text="Click me!", width=50, height=10, master=root)
+# label.pack()
+# label.bind("<Button-1>", command)
 
-root.mainloop()
+# root.mainloop()
+
+
+def cal(a, b=3):
+
+    sum = a + b
+
+    sub = a - b
+
+    mul = a * b
+
+    return sum, sub, mul
+
+s, sub, mul = cal(6)
+
+print(s, mul, sub)
