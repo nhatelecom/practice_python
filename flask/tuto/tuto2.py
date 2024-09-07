b73de8be2ed46a2399fdc945a45f1b362a54d2bf
nhatelecom <nhatelecom@gmail.com>
2024-09-07 15:09:25 +0700

    .
    .

--- flask/tuto/tuto2.py
+++ flask/tuto/tuto2.py
@@ -1,15 +1,14 @@
 # Youtube: Tech with Tim
+# Turorial 2
+# https://www.youtube.com/watch?v=xIgPMguqyws&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=2
 
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-# Create page
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name)
 
 if __name__ == "__main__":
     app.run()
